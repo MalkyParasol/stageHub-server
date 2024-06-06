f7c8812cfdcd62786502b1e06f5d4e8e097c3af7
unknown <malky5885+git@gmail.com>
2024-06-06 17:28:13 +0300

    adding app and actor model

---
+++ models/actor.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ActorSchema =  new mongoose.Schema({
+    name:String,
+    role:String,
+    coachId:String,
+    directorId:String,
+    phone:String,
+    email:String
+})
+
+module.exports = mongoose.model("Actors",ActorSchema);
